d526fa229e967aa4aeebac9bfb8c8197bcb8fa03
Muxuan Liang <mliang@fredhutch.org>
2020-06-19 23:45:28 -0700

    update documentation

--- R/itrInferenceAll.R
+++ R/itrInferenceAll.R
@@ -1,6 +1,6 @@
 #' Estimation and inference for individualized treatment rule using pooled-and-split de-correlated score
 #'
-#' This function
+#' This function implements the EARL estimation for individualized treatment rule and the inference procedure based on the pooled-and-split de-correlated score (see reference).
 #'
 #' @param data A list - list(predictor = x, treatment = trt, outcome = y)), where x is the covariate matrix, trt is 0 or 1 (1 indicates treatment), y is the outcome.
 #' @param propensity estimated propensity score p(trt=1|X). This should be used only when the propensity is estimated by a parametric model.
@@ -13,10 +13,29 @@
 #' @param parallel whether use parallel computing; by default, FALSE.
 #' @param screeningMethod when dimension is high, this selects method to preform variable screening for outcomeModel and propensityModel fitting. Methods in VariableScreening package are available.
 #' @param standardize whether standardize the input covariant matrix.
-#' @return A matrix of the infile
+#' @return fit is estimated coefficient for ITR on two split datasets. p-values are the p-value for each coefficients included in indexToTest. (betaAN-1.96*sigmaAN/sqrt(sample size), betaAN+1.96*sigmaAN/sqrt(sample size)) provides the 95\% CI for the coefficients.
 #' @author Muxuan Liang <mliang@fredhutch.org>
 #' @references To be added
-#' @example
+#' @examples
+#' # generate data
+#' n <- 500
+#' p <- 800
+#' x <- array(rnorm(n*p), c(n,p))
+#' beta_pi <- 0.4*c(1,1,1,0,-1,0,-1,rep(0, times=p-7))
+#' trt <- apply(x, 1, function(t){ rbinom(1, 1, prob = exp(t[1]^2+t[2]^2+t[1]*t[2])/(1+exp(t[1]^2+t[2]^2+t[1]*t[2])))})
+#' beta_main <- 0.8*c(-1,-1,1,-1,0,0,rep(0, times=p-6))
+#' main_effect <- exp(x %*% beta_main)
+#' beta_inter <- 0.7*c(1,1,-1,-1,rep(0, times=p-4))
+#' interact_effect <- (pnorm(x %*% beta_inter)-0.5)*(abs(apply(x[,c(4:8)],1,sum)) + 4 * xi)
+#' e <- rnorm(n)
+#' y <- main_effect + sign(trt - 0.5) * interact_effect + e
+#' # set up training data
+#' dataTrain <- list(predictor = x, treatment = (trt > 0), outcome = y)
+#' # fit our approach
+#' fit_itrAll <- ITRFitAll(data = dataTrain, outcomeModel = 'kernel', propensityModel = 'kernel')
+#' # compare with q-learning and implement standard de-correlated score for q0learning
+#' fit_qlearn <- QLearnFit(data = dataTrain, intercept = TRUE)
+#' score_qlearn <- scoreTestQLearn(fit_qlearn, parallel=FALSE)
 #' @export
 
 ITRFitAll <- function(data, propensity = NULL, outcome = NULL, loss = 'logistic', sampleSplitIndex=NULL,
@@ -48,7 +67,7 @@ ITRFitAll <- function(data, propensity = NULL, outcome = NULL, loss = 'logistic'
     sigma <- sqrt((score_1$sigma^2 + score_2$sigma^2)/2)
     I <- (score_1$I+score_2$I)/2
     sigmaAN <- sigma/I
-    res <- list(fit =fit, score = score, sigma=sigma, pvalue=pnorm(-abs(sqrt(size)*score/sigma))*2, betaAN=betaAN, sigmaAN=sigmaAN)
+    res <- list(fit =fit, pvalue=pnorm(-abs(sqrt(size)*score/sigma))*2, betaAN=betaAN, sigmaAN=sigmaAN)
   } else {
     res <- list(fit = fit)
   }
